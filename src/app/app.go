7dfaf5d5420eb8dfbec72c808956c60ca52ec534
andrymahdison <andreymahdison@gmail.com>
2019-04-04 17:13:19 +0700

    dispatch router

--- src/app/app.go
+++ src/app/app.go
@@ -1,15 +1,36 @@
 package app
 
 import(
+	"database/sql"
 	"net/http"
 
-	"github.com/labstack/echo"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo"	
+	"github.com/summon23/go-echo-rest-boilerplate/src/app/controller"
 )
 
+func dbConn() (db *sql.DB) {
+	db, err := sql.Open("mysql", "root:@/testo")
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+// StartServer run the server
 func StartServer() {
 	e := echo.New()
-	e.GET("/", func (c echo.Context) error {
-		return c.String(http.StatusOK, "hell")
-	})
-	e.Logger.Fatal(e.Start(":1312"))
+
+	registerMethod(User)
+	db := dbConn()
+
+	userHandler := controller.User{}
+	userHandler.RegisterHandler(db, e)
+	// e.GET("/", func (c echo.Context) error {
+	// 	return c.String(http.StatusOK, "hell")
+	// })
+
+	// e.GET("/test", getData)
+	e.GET("/user", GetAllUser)
+	e.Logger.Fatal(e.Start(":1322"))
 }
